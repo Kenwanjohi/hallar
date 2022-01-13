91b40c848caa441ec7dd251cc2980576f1af74dd
Kenwanjohi <wanjohiken85@gmail.com>
2022-01-13 23:32:55 +0300

    Add chakra nav template

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,34 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+
+import { ChakraProvider , extendTheme} from '@chakra-ui/react'
+
+const theme = extendTheme({
+  styles: {
+    global: {
+      // styles for the `body`
+      body: {
+        fontFamily: `'Signika', sans-serif`
+      },
+      // styles for the `a`
+      // a: {
+      //   color: 'teal.500',
+      //   _hover: {
+      //     textDecoration: 'underline',
+      //   },
+      // },
+    },
+  },
+})
+
 import './index.css'
 import App from './App'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider theme={theme} >
+      <App />
+    </ChakraProvider>
   </React.StrictMode>,
   document.getElementById('root')
 )
