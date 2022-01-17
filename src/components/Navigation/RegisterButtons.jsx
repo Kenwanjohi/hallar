8cf3651078a24e441292ff4fe2e27d1595c2f71a
Kenwanjohi <wanjohiken85@gmail.com>
2022-01-17 20:40:43 +0300

    Register buttons

---
+++ src/components/Navigation/RegisterButtons.jsx
@@ -0,0 +1,10 @@
+import { Stack, Button } from '@chakra-ui/react'
+
+export function RegisterButtons() {
+    return (
+        <Stack direction={'row'}>
+            <Button variant={'outline'} borderColor={'pink.500'}  fontSize={'sm'} fontWeight={600} _hover={{ bg: 'pink.500', color: 'white'}}>Sign in</Button>
+            <Button bg={'pink.500'} color={'white'} fontSize={'sm'} fontWeight={600}  _hover={{ bg: 'pink.300'}}>Sign up</Button>
+        </Stack>            
+    )
+}
