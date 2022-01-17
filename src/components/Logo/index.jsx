64029c01b12a6ce34463a680b51b582c88a8c84e
Kenwanjohi <wanjohiken85@gmail.com>
2022-01-17 20:46:03 +0300

    Responsive navigation Dark-light theme switch Menu-mobile

---
+++ src/components/Logo/index.jsx
@@ -0,0 +1,7 @@
+import { Heading } from '@chakra-ui/react'
+
+export function Logo() {
+    return (
+        <Heading as='a' cursor={'pointer'} fontSize={25}>trouver</Heading>
+    )
+}
---
+++ src/components/Navigation/index.jsx
@@ -0,0 +1,45 @@
+import { Box, Flex, HStack, IconButton, Collapse, useDisclosure, useColorMode, useColorModeValue} from '@chakra-ui/react'
+import { HamburgerIcon, CloseIcon, MoonIcon, SunIcon} from '@chakra-ui/icons'
+import { Logo } from '../Logo'
+import { DesktopNav } from './DesktopNav'
+import { MobileNav } from './MobileNav'
+import { RegisterButtons } from './RegisterButtons'
+const navItems = [
+  {
+    label: 'TV SHOWS',
+    href: '#',
+  },
+  {
+    label: 'MOVIES',
+    href: '#',
+  },
+  {
+    label: 'TRENDING',
+    href: '#',
+  }
+];
+export function Navigation() {
+    const { isOpen: isMobileNavOpen, onToggle } = useDisclosure()
+    const { colorMode, toggleColorMode } = useColorMode()
+    
+    return (
+        <Box>
+            <Flex py='10px' px='20px' align={'center'} justify={'space-between'}  borderBottom={1} borderStyle={'solid'} borderColor={useColorModeValue('gray.200', 'gray.900')}>
+                <HStack spacing='40px' >
+                    <Logo/>
+                    <DesktopNav navItems={navItems}/>
+                </HStack>
+                <HStack>
+                    <IconButton aria-label={'Switch theme'} onClick={toggleColorMode}  icon={colorMode === 'light' ? <MoonIcon /> : <SunIcon />}/>
+                    <IconButton aria-label={'Open-Close menu'} onClick={onToggle} display={{ md: 'none' }} icon={ isMobileNavOpen ? <CloseIcon w={3} h={3} /> : <HamburgerIcon w={5} h={5} />} />
+                    <Box display={{ base: 'none', md: 'block' }}>
+                      <RegisterButtons />
+                    </Box>
+                </HStack>
+            </Flex>
+            <Collapse in={isMobileNavOpen} animateOpacity>
+              <MobileNav navItems={navItems}/>
+            </Collapse>
+        </Box>
+    )
+}
