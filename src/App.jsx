04ed4db1970c45e166b7380a88c05373ed9a6bae
Kenwanjohi <wanjohiken85@gmail.com>
2022-01-18 22:45:12 +0300

    React Query config

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,24 @@
 import { Box } from '@chakra-ui/react'
 import { Main } from './components/Main'
 import { Navigation } from './components/Navigation'
+import {
+  QueryClient,
+  QueryClientProvider,
+} from 'react-query'
 
+ // Create a client
+ const queryClient = new QueryClient()
 function App() {
 
+
   return (
-    <Box>
-      <Navigation/>
-      <Box w='100%' h='60vh' bgGradient='linear(135deg, rgb(45, 12, 63) 0%, rgb(148, 79, 110) 50%, rgb(239, 146, 118) 100%)' />
-      <Main/>
-    </Box>
+    <QueryClientProvider client={queryClient}>
+      <Box>
+        <Navigation/>
+        <Box w='100%' h='60vh' bgGradient='linear(135deg, rgb(45, 12, 63) 0%, rgb(148, 79, 110) 50%, rgb(239, 146, 118) 100%)' />
+        <Main/>
+      </Box>
+    </QueryClientProvider>
   )
 }
 
