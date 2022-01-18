eabc5739c7a0a4380b8298e7ed6a8bac247dea7c
Kenwanjohi <wanjohiken85@gmail.com>
2022-01-18 22:17:26 +0300

    Add categories section

---
+++ src/components/Main/CategorySection.jsx
@@ -0,0 +1,77 @@
+import { useState } from 'react'
+import { Box, IconButton } from '@chakra-ui/react'
+import { ArrowLeftCircle, ArrowRightCircle } from 'react-feather';
+import { PosterCard } from './PosterCard'
+
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
+const settings = {
+    arrows: false,
+    dots: true,
+    infinite: false,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 4,
+    variableWidth: true,
+    responsive: [
+        {
+          breakpoint: 1216,
+          settings: {
+            slidesToScroll: 3,
+          }
+        },
+        {
+          breakpoint: 924,
+          settings: {
+            slidesToScroll: 2,
+          }
+        },
+        {
+          breakpoint: 656,
+          settings: {
+            slidesToScroll: 1
+          }
+        }
+      ]
+};
+
+export function CategorySection(props) {
+    const [slider, setSlider] = useState(null);
+    return (
+        <Box as={'section'} position={'relative'}>
+            <IconButton
+                aria-label="left-arrow"
+                position="absolute"
+                left='-15px'
+                top='40%'
+                transform={'translate(0%, -50%)'}
+                zIndex={2}
+                onClick={() => slider?.slickPrev()}>
+                <ArrowLeftCircle />
+            </IconButton>
+            {/* Right Icon */}
+            <IconButton
+                aria-label="right-arrow"
+                position="absolute"
+                right='-15px'
+                top='40%'
+                transform={'translate(0%, -50%)'}
+                zIndex={2}
+                onClick={() => slider?.slickNext()}>
+                <ArrowRightCircle />
+            </IconButton>
+            <Slider {...settings} ref={(slider) => setSlider(slider)}>
+                <PosterCard/>
+                <PosterCard/>
+                <PosterCard/>
+                <PosterCard/>
+                <PosterCard/>
+                <PosterCard/>
+                <PosterCard/>
+                <PosterCard/>
+                <PosterCard/>
+            </Slider>
+        </Box>
+    )
+}
