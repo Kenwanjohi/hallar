ba36e5d14ea7999706688b4fa05ac8b822f74be0
Kenwanjohi <wanjohiken85@gmail.com>
2022-01-18 22:18:56 +0300

    Render Main Section

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { Box } from '@chakra-ui/react'
+import { Main } from './components/Main'
 import { Navigation } from './components/Navigation'
 
 function App() {
@@ -6,7 +7,8 @@ function App() {
   return (
     <Box>
       <Navigation/>
-      <Box w='100%' h='70vh' bgGradient='linear(135deg, rgb(45, 12, 63) 0%, rgb(148, 79, 110) 50%, rgb(239, 146, 118) 100%)' />
+      <Box w='100%' h='60vh' bgGradient='linear(135deg, rgb(45, 12, 63) 0%, rgb(148, 79, 110) 50%, rgb(239, 146, 118) 100%)' />
+      <Main/>
     </Box>
   )
 }
