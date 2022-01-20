a87cb164156f629af7d91bd051873ea93951878c
Kenwanjohi <wanjohiken85@gmail.com>
2022-01-20 12:59:56 +0300

    fIx slow render

--- src/App.jsx
+++ src/App.jsx
@@ -7,10 +7,14 @@ import {
 } from 'react-query'
 
  // Create a client
- const queryClient = new QueryClient()
+ const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+})
 function App() {
-
-
   return (
     <QueryClientProvider client={queryClient}>
       <Box>
