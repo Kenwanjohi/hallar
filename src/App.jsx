56a4f124d83930b36fafb02a9e3deddc85330f89
Kenwanjohi <wanjohiken85@gmail.com>
2022-01-21 14:32:02 +0300

    Add home route

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,44 @@
-import { Box, Flex, Text } from '@chakra-ui/react'
-import { Main } from './components/Main'
-import { Navigation } from './components/Navigation'
-import {
-  QueryClient,
-  QueryClientProvider,
-} from 'react-query'
+import { Box, Flex, Text } from '@chakra-ui/react';
+import { Main } from './components/Main';
+import { Navigation } from './components/Navigation';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { Routes, Route } from 'react-router-dom';
 
- // Create a client
- const queryClient = new QueryClient({
+// Create a client
+const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
-      refetchOnWindowFocus: false,
-    },
-  },
-})
+      refetchOnWindowFocus: false
+    }
+  }
+});
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <Box>
-        <Navigation/>
-        <Flex direction={'column'} justify={'center'} align={'center'} w='100%' h='60vh' bgGradient='linear(135deg, rgb(45, 12, 63) 0%, rgb(148, 79, 110) 50%, rgb(239, 146, 118) 100%)'>
-          <Text fontSize={'20px'}>Discover your favorite movies and tv shows</Text>
-        </Flex>
-        <Main/>
+        <Navigation />
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
+                <Flex
+                  direction={'column'}
+                  justify={'center'}
+                  align={'center'}
+                  w="100%"
+                  h="60vh"
+                  bgGradient="linear(135deg, rgb(45, 12, 63) 0%, rgb(148, 79, 110) 50%, rgb(239, 146, 118) 100%)">
+                  <Text fontSize={'20px'}>Discover your favorite movies and tv shows</Text>
+                </Flex>
+                <Main />
+              </>
+            }
+          />
+        </Routes>
       </Box>
     </QueryClientProvider>
-  )
+  );
 }
 
-export default App
+export default App;
