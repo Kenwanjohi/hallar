8574a4188bd77caec4fdc25b5d4333675d4b8a5d
Kenwanjohi <wanjohiken85@gmail.com>
2022-01-27 18:49:05 +0300

    Api changes

--- src/api.js
+++ src/api.js
@@ -11,16 +11,20 @@ export const endPoints = {
   ontheair: 'on_the_air'
 };
 
-async function fetchMoviesTvShows(category, type , pageParam) {
+async function fetchMoviesTvShows(category, type = 'movie', pageParam = 1) {
   let response = await axios.get(
-    `${BASE_URL}${type}/${endPoints[`${category}`]}?api_key=${apikey}&language=en-US&page=${pageParam}`
+    `${BASE_URL}${type}/${
+      endPoints[`${category}`]
+    }?api_key=${apikey}&language=en-US&page=${pageParam}`
   );
   return response.data;
 }
 
-async function fetchTrends(type) {
-  let response = await axios.get(`${BASE_URL}trending/${type}/week?api_key=${apikey}`);
+async function fetchTrends(type, pageParam = 1) {
+  let response = await axios.get(
+    `${BASE_URL}trending/${type}/week?api_key=${apikey}&page=${pageParam}`
+  );
   return response.data;
 }
 
-export {fetchMoviesTvShows, fetchTrends}
+export { fetchMoviesTvShows, fetchTrends };
