aaa20917436adcd29a38d4934e0ecacc7d3baf02
Kenwanjohi <wanjohiken85@gmail.com>
2022-01-28 02:34:12 +0300

    Add desktop nav items

--- src/components/Navigation/DesktopNav.jsx
+++ src/components/Navigation/DesktopNav.jsx
@@ -1,14 +1,77 @@
-import { Box, HStack, Link } from '@chakra-ui/react'
-export  function DesktopNav({navItems}) {
+import {
+  Box,
+  HStack,
+  Stack,
+  Flex,
+  Link,
+  Popover,
+  PopoverTrigger,
+  PopoverContent,
+  Text,
+  Icon,
+  useColorModeValue
+} from '@chakra-ui/react';
+import { ChevronRightIcon } from '@chakra-ui/icons';
+export function DesktopNav({ navItems }) {
+  return (
+    <Box as={'nav'} display={{ base: 'none', md: 'block' }}>
+      <HStack>
+        {navItems.map((item, i) => (
+          <Box key={i}>
+            <Popover trigger={'hover'} placement={'bottom-start'}>
+              <PopoverTrigger>
+                <Text p={2} fontSize={'xs'} cursor={'pointer'} fontWeight={700}>
+                  {item.label}
+                </Text>
+              </PopoverTrigger>
 
-    return (
-        <Box as={'nav'}  display={{ base: 'none', md: 'block' }}>
-            <HStack>
-                {navItems.map((item,i) => (
-                  <Link key={i} fontSize={'xs'} fontWeight={700}>{item.label}</Link>
-                ))
-                }
-            </HStack>
-        </Box>
-    )
+              <PopoverContent
+                border={0}
+                boxShadow={'xl'}
+                bg={useColorModeValue('white', 'gray.800')}
+                p={2}
+                rounded={'xl'}
+                >
+                <Stack>
+                  {item.links.map((linkItem, i) => (
+                    <DesktopNavItem key={i} {...linkItem} />
+                  ))}
+                </Stack>
+              </PopoverContent>
+            </Popover>
+          </Box>
+        ))}
+      </HStack>
+    </Box>
+  );
 }
+
+const DesktopNavItem = ({ link, href }) => {
+  return (
+    <Link
+      href={href}
+      role={'group'}
+      display={'block'}
+      p={2}
+      rounded={'md'}
+      _hover={{ bg: useColorModeValue('pink.50', 'gray.900') }}>
+      <Stack direction={'row'} align={'center'}>
+        <Box>
+          <Text fontSize={'xs'} transition={'all .3s ease'} _groupHover={{ color: 'pink.500' }} fontWeight={700}>
+            {link}
+          </Text>
+        </Box>
+        <Flex
+          transition={'all .3s ease'}
+          transform={'translateX(-10px)'}
+          opacity={0}
+          _groupHover={{ opacity: '100%', transform: 'translateX(0)' }}
+          justify={'flex-end'}
+          align={'center'}
+          flex={1}>
+          <Icon color={'pink.500'} w={5} h={5} as={ChevronRightIcon} />
+        </Flex>
+      </Stack>
+    </Link>
+  );
+};
