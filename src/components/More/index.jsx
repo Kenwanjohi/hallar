fa3165f2627b0b62d3048441fbb0a12b8e6537ab
Kenwanjohi <wanjohiken85@gmail.com>
2022-01-27 18:48:33 +0300

    see more, infinite scroll

--- src/components/More/index.jsx
+++ src/components/More/index.jsx
@@ -3,41 +3,59 @@ import { Box, Grid, GridItem } from '@chakra-ui/react';
 import { fetchMoviesTvShows, fetchTrends } from '../../api';
 import { useLocation, useParams } from 'react-router-dom';
 import { useInfiniteQuery } from 'react-query';
-import { PosterCard  } from '../Main/PosterCard'
+import { PosterCard } from '../Main/PosterCard';
+import InfiniteScroll from 'react-infinite-scroll-component';
 export function More() {
   const { section } = useParams();
   const { pathname } = useLocation();
   const mediaType = pathname.slice(1, pathname.lastIndexOf('/'));
 
-  const { data, fetchNextPage } = useInfiniteQuery(
-    section,
-    ({ pageParam = 1 }) => fetchMoviesTvShows(section, mediaType, pageParam),
-    {
-      getNextPageParam: (lastpage) => {
-        const { page, total_pages: totalPages } = lastpage;
-        console.log(page);
-        return page < totalPages ? page + 1 : undefined;
+  const { data, error, fetchNextPage, hasNextPage, isFetching, isFetchingNextPage, status } =
+    useInfiniteQuery(
+      section,
+      ({ pageParam = 1 }) => {
+        if (section === 'tvTrends' || section === 'movieTrends') {
+          return fetchTrends(mediaType, pageParam);
+        }
+        return fetchMoviesTvShows(section, mediaType, pageParam);
+      },
+      {
+        getNextPageParam: (lastpage) => {
+          const { page, total_pages: totalPages } = lastpage;
+          return page < totalPages ? page + 1 : undefined;
+        },
+        refetchOnMount: true
       }
-    }
-  );
-  // const data = useInfiniteQuery(section, () => fetchTrends(path), {
-  //   getNextPageParam: (lastpage, pages) => lastpage.nextPage
-  // });
-  // console.log(data)
-  console.log(data);
+    );
+
+  if (status === 'loading') return <p>Loading...</p>;
+  if (status === 'error') return <span>Error: {error.message}</span>;
+  const totalItems = (data?.pages ?? []).map((group) => group.results).flat(1).length;
+
   return (
     <Box pt="60px">
-      <Grid templateColumns= {{base:'repeat(2, 1fr)', md: 'repeat(3, 1fr)'}} gap={6}>
-        {data && data.pages.map((group, i) => (
-          <React.Fragment key={i}>
-            {group.results.map((item) => (
-              <GridItem w='100%' key={item.id} >
-                <PosterCard details={item} />
-              </GridItem>
-            ))}
-          </React.Fragment>
-        ))}
-      </Grid>
+      <InfiniteScroll
+        dataLength={totalItems}
+        next={fetchNextPage}
+        hasMore={hasNextPage}
+        loader={<h4>Loading...</h4>}
+        endMessage={
+          <p style={{ textAlign: 'center' }}>
+            <b>Yay! You have seen it all</b>
+          </p>
+        }>
+        <Grid templateColumns="repeat(auto-fit, minmax(250px, 1fr))" gap={6}>
+          {(data?.pages ?? []).map((group, i) => (
+            <React.Fragment key={i}>
+              {group.results.map((item) => (
+                <GridItem key={item.id}>
+                  <PosterCard height="initial" width="initial" details={item} />
+                </GridItem>
+              ))}
+            </React.Fragment>
+          ))}
+        </Grid>
+      </InfiniteScroll>
     </Box>
   );
 }
