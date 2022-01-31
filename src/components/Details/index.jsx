b2366657ba4428951a613038339393f0f732c6b5
Kenwanjohi <wanjohiken85@gmail.com>
2022-01-31 14:18:49 +0300

    Poster Image quality

--- src/components/Details/index.jsx
+++ src/components/Details/index.jsx
@@ -62,7 +62,7 @@ export function Details() {
           <HStack position='relative' h='max-content' align={{base: 'end', md: 'center'}}>
             <Box width={{ base: '100%', md: '500px'}} m={{md: '50px 0px 50px 50px'}} >
                 <img
-                  src={`https://image.tmdb.org/t/p/w500${poster_path}`}
+                  src={`https://image.tmdb.org/t/p/original${poster_path}`}
                   width="100%"
                   alt="Movie poster"
                 />
