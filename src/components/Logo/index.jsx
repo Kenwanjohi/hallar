f8d694a34ea0ad7bd81ab558833988362801a9d6
Kenwanjohi <wanjohiken85@gmail.com>
2022-02-04 00:02:56 +0300

    Add home Link

--- src/components/Logo/index.jsx
+++ src/components/Logo/index.jsx
@@ -1,3 +1,8 @@
+import { Link } from 'react-router-dom';
 export function Logo() {
-  return <h1 style={{ fontSize: '25px', fontWeight: '700' }}>hallar</h1>;
+  return (
+    <Link to={'/'} >
+      <h1 style={{ fontSize: '25px', fontWeight: '700' }}>hallar</h1>
+    </Link>
+  );
 }
